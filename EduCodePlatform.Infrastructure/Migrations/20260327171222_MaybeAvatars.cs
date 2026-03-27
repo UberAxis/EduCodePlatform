8c3d26156461200edf758fc819947b99a5ea4c8d
leenzery <ly@leenzery.dev>
2026-03-27 22:12:45 +0500

    avatars migration

---
+++ EduCodePlatform.Infrastructure/Migrations/20260327171222_MaybeAvatars.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduCodePlatform.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MaybeAvatars : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AvatarUrl",
+                table: "Users",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AvatarUrl",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+    }
+}
